8b710a276b5a165a546b29f06c191d7266a895ab
mikinovation <maneuver2472@gmail.com>
2024-06-09 16:19:26 +0800

    feat: add feed page

--- packages/realworld/packages/front/nuxt.config.ts
+++ packages/realworld/packages/front/nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ["@/assets/css/main.css"],
+  components: {
+    dirs: [
+      {
+        path: "@/widgets",
+        prefix: "Widget",
+      },
+    ],
+  },
 });
---
+++ packages/realworld/packages/front/widgets/feed/FeedPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div className="home-page">
+    <div className="banner">
+      <div className="container">
+        <h1 className="logo-font">conduit</h1>
+        <p>A place to share your knowledge.</p>
+      </div>
+    </div>
+  </div>
+</template>
