8a738bed1880b06349cc7d7c7ca2dad9d11fcd2c
mikinovation <maneuver2472@gmail.com>
2024-06-16 09:37:19 +0800

    feat: init rust

---
+++ packages/realworld/target/debug/deps/api-698a4c2b5bf75b05.d
@@ -0,0 +1,5 @@
+/home/mikinovation/projects/mikinovation/packages/realworld/target/debug/deps/api-698a4c2b5bf75b05: api/src/main.rs
+
+/home/mikinovation/projects/mikinovation/packages/realworld/target/debug/deps/api-698a4c2b5bf75b05.d: api/src/main.rs
+
+api/src/main.rs:
