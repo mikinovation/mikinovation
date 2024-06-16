9a3c00f8e27d1784421a8b1d11f1d58bcf9a4559
Mikihiro Saito <33025073+mikinovation@users.noreply.github.com>
2024-06-16 15:09:43 +0800

    chore: refactor routers (#36)
    * chore: refactor routers
    
    * fix: format

--- packages/realworld/api/src/main.rs
+++ packages/realworld/api/src/main.rs
@@ -1,75 +1,14 @@
-use axum::{
-    http::StatusCode,
-    routing::{get, post},
-    Json, Router,
-    http::{header, Method, Request}
-};
-use serde::{Deserialize, Serialize};
+mod routers;
+
 use tracing_subscriber;
+use routers::{AppRouter};
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let app = create_app();
+    let app = AppRouter::new();
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3418").await.unwrap();
     axum::serve(listener, app).await.unwrap();
-}
-
-fn create_app() -> Router {
-    Router::new()
-        .route("/", get(root))
-        .route("/users", post(create_user))
-}
-
-async fn root() -> &'static str {
-    "Hello, World!"
-}
-
-async fn create_user(
-    Json(payload): Json<CreateUser>,
-) -> (StatusCode, Json<User>) {
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
-
-    (StatusCode::CREATED, Json(user))
-}
-
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
-}
-
-#[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
-}
-
-#[cfg(test)]
-mod test {
-  use super::*;
-  use tower::ServiceExt;
-  use http_body_util::BodyExt;
-  use axum::body::Body;
-
-  #[tokio::test]
-  async fn test_root() {
-     let request = Request::builder()
-         .method(Method::GET)
-         .uri("/")
-         .body(Body::empty())
-         .unwrap();
-
-     let response = create_app()
-         .oneshot(request)
-         .await
-         .unwrap();
-
-      assert_eq!(response.status(), StatusCode::OK);
-      assert_eq!(response.into_body().collect().await.unwrap().to_bytes(), "Hello, World!");
-  }
 }
