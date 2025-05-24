431f142c2b07c24990159682936a54f17b7a7abf
mikinovation <maneuver2472@gmail.com>
2025-05-24 22:02:52 +0800

    fix: reorder nuxt config keys to comply with ESLint rules

--- packages/web/nuxt.config.ts
+++ packages/web/nuxt.config.ts
@@ -1,22 +1,22 @@
 import { ViteMcp } from 'vite-plugin-mcp'
 
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
   modules: ['@nuxt/eslint', '@nuxt/test-utils', 'nuxt-mcp'],
   // TODO: SSR is disabled because of the API mocking
   ssr: !process.env.NUXT_PUBLIC_API_MOCK,
   devtools: { enabled: true },
-  vite: {
-    plugins: [
-      ViteMcp()
-    ],
-  },
   runtimeConfig: {
     githubApiToken: process.env.GITHUB_API_TOKEN,
     public: {
       apiMock: process.env.NUXT_PUBLIC_API_MOCK,
       apiUrl: process.env.NUXT_PUBLIC_API_URL || 'http://localhost:3333',
     },
+  },
+  compatibilityDate: '2024-11-01',
+  vite: {
+    plugins: [
+      ViteMcp(),
+    ],
   },
   eslint: {
     config: {
