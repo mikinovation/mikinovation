09b8df4f9a23c3524b9da72f9434ae8933077fa1
mikinovation <maneuver2472@gmail.com>
2025-05-24 21:45:47 +0800

    feat: configure MCP integration for Nuxt and Vite applications

--- packages/web/nuxt.config.ts
+++ packages/web/nuxt.config.ts
@@ -1,8 +1,16 @@
+import { ViteMcp } from 'vite-plugin-mcp'
+
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/test-utils'],
+  compatibilityDate: '2024-11-01',
+  modules: ['@nuxt/eslint', '@nuxt/test-utils', 'nuxt-mcp'],
   // TODO: SSR is disabled because of the API mocking
   ssr: !process.env.NUXT_PUBLIC_API_MOCK,
   devtools: { enabled: true },
+  vite: {
+    plugins: [
+      ViteMcp()
+    ],
+  },
   runtimeConfig: {
     githubApiToken: process.env.GITHUB_API_TOKEN,
     public: {
@@ -10,7 +18,6 @@ export default defineNuxtConfig({
       apiUrl: process.env.NUXT_PUBLIC_API_URL || 'http://localhost:3333',
     },
   },
-  compatibilityDate: '2024-11-01',
   eslint: {
     config: {
       stylistic: true,
